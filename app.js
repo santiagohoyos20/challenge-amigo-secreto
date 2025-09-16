b9478ca303940e79820f485e88872540f9fe0b80
Santiago Hoyos <santiagojhoyoss@gmail.com>
2025-09-15 22:49:48 -0500

    done

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 const inputField = document.getElementById("amigo");
-const lista = document.getElementById("listaAmigos")
+const lista = document.getElementById("listaAmigos");
+const resultado = document.getElementById("resultado");
 
 let amigos = [];
+let amigoSorteado = "";
 
 const agregarAmigo = () => {
   if (inputField.value) {
     let nuevoAmigo = inputField.value;
     amigos.push(nuevoAmigo);
-    inputField.value = ""
-    actualizarAmigos()
+    inputField.value = "";
+    actualizarAmigos();
   } else {
     alert("Por favor, inserte un nombre.");
   }
 };
 
-
 const actualizarAmigos = () => {
-    lista.innerHTML = ""
-    lista.innerHTML = amigos.map((amigo) => `<li>${amigo}</li>`).join("")
-}
+  lista.innerHTML = "";
+  lista.innerHTML = amigos.map((amigo) => `<li>${amigo}</li>`).join("");
+};
+
+const sortearAmigo = () => {
+  if (amigos.length === 0) {
+    return;
+  }
+  let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  amigoSorteado = amigos[indiceAleatorio];
+  mostrarAmigoSorteado()
+};
+
+const mostrarAmigoSorteado = () => {
+  lista.innerHTML = "";
+  resultado.innerHTML = `El amigo elegido es: ${amigoSorteado}`;
+};
