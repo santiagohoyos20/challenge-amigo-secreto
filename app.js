69c00a8266a725bebbe8ba8d47823c2e13deee69
Santiago Hoyos <santiagojhoyoss@gmail.com>
2025-09-15 22:33:30 -0500

    solo falta 1 funcion

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const inputField = document.getElementById("amigo");
+const lista = document.getElementById("listaAmigos")
+
+let amigos = [];
+
+const agregarAmigo = () => {
+  if (inputField.value) {
+    let nuevoAmigo = inputField.value;
+    amigos.push(nuevoAmigo);
+    inputField.value = ""
+    actualizarAmigos()
+  } else {
+    alert("Por favor, inserte un nombre.");
+  }
+};
+
+
+const actualizarAmigos = () => {
+    lista.innerHTML = ""
+    lista.innerHTML = amigos.map((amigo) => `<li>${amigo}</li>`).join("")
+}
